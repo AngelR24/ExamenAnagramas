c66667b803c3755cc481ed7da0eb94f64e610cb2
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2020-01-15 02:01:07 -0400

    first commit

---
+++ AnagramChecker/AnagramChecker/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnagramChecker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public List<string> AnagramChecker(List<string> wordList)
+        {
+            return null;
+        }
+    }
+}
---
+++ AnagramChecker/AnagramCheckerTests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using AnagramChecker;
+using NUnit.Framework;
+
+namespace AnagramCheckerTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public void NullListReturnsNull()
+        {
+            List<string> wordList = null;
+
+            Program program = new Program();
+
+            List<string> anagramList = program.AnagramChecker(wordList);
+
+            Assert.AreEqual(wordList, anagramList);
+        }
+    }
+}
